f9955c5cde35f396dc1faeafc9bce205face674b
sqz777 <sqz777isme@gmail.com>
2018-07-16 22:37:50 +0800

    let launchUrl empty and set startup

--- PlayGround/Startup.cs
+++ PlayGround/Startup.cs
@@ -34,6 +34,22 @@ namespace PlayGround
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404 &&                       // 該資源不存在
+                    !System.IO.Path.HasExtension(context.Request.Path.Value) && // 網址最後沒有帶副檔名
+                    !context.Request.Path.Value.StartsWith("/api"))             // 網址不是 /api 開頭（不是發送 API 需求）
+                {
+                    context.Request.Path = "/index.html";                       // 將網址改成 /index.html
+                    context.Response.StatusCode = 200;                          // 並將 HTTP 狀態碼修改為 200 成功
+
+                    await next();
+                }
+            });
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
