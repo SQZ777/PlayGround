002af094d1d0889be53054993664e7deb98b3ebe
sqz777 <sqz777isme@gmail.com>
2018-07-18 23:03:15 +0800

    you can input something!

--- PlayGround/ClientApp/src/app/app.component.ts
+++ PlayGround/ClientApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {MESSAGES} from './mock-msg';
+import { MESSAGES } from './mock-msg';
+import { findLast } from '../../node_modules/@angular/compiler/src/directive_resolver';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +9,10 @@ import {MESSAGES} from './mock-msg';
 export class AppComponent {
   title = 'app';
   messages = MESSAGES;
+  inputMsg = '';
+
+  addMessage(): void {
+    this.messages.push({ msg: this.inputMsg });
+    this.inputMsg = '';
+  }
 }
