50dfbb67563bb9ad87ab5fbb2bdefe04f9fc1829
sqz777 <sqz777isme@gmail.com>
2018-07-20 08:16:49 +0800

    move original message to now message

--- PlayGround/ClientApp/src/app/app-routing.module.ts
+++ PlayGround/ClientApp/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { MessageListComponent } from './message-list/message-list.component';
 
-@NgModule({
-  exports: [RouterModule]
-})
+const routes: Routes = [
+  { path: 'messageList', component: MessageListComponent }
+];
+
+@NgModule(
+  { exports: [RouterModule] })
 
 export class AppRoutingModule { }
--- PlayGround/ClientApp/src/app/app.module.ts
+++ PlayGround/ClientApp/src/app/app.module.ts
@@ -4,14 +4,12 @@ import {FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { MessageRecordComponent } from './message-record/message-record.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { MessageListComponent } from './message-list/message-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessageRecordComponent,
     MessageListComponent
   ],
   imports: [
