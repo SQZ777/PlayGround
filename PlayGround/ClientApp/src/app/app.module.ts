52ba32f531d5adfc9c8a02a7e3f6276f48af105b
sqz777 <sqz777isme@gmail.com>
2018-07-20 08:04:11 +0800

    add route

--- PlayGround/ClientApp/src/app/app.module.ts
+++ PlayGround/ClientApp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MessageRecordComponent } from './message-record/message-record.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MessageRecordComponent } from './message-record/message-record.componen
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
