841e0bcc64d5824edc4487a00e5999d3b7814ba8
sqz777 <sqz777isme@gmail.com>
2018-07-22 02:28:07 +0800

    add a post message!

--- PlayGround/ClientApp/src/app/message.service.ts
+++ PlayGround/ClientApp/src/app/message.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Message } from './message';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable({
   providedIn: 'root'
 })
+
 export class MessageService {
   private messageUrl = 'http://localhost:1974/api/Messages';
 
   constructor(private http: HttpClient) { }
-
   getMsgs(): Observable<Message[]> {
     return this.http.get<Message[]>(this.messageUrl);
+  }
+
+  addMsg(msg: Message): Observable<Message> {
+    return this.http.post<Message>(this.messageUrl, msg, httpOptions);
   }
 
 }
