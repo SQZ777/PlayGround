59af1c73ecd60661f7155c856a68addf554a5afd
sqz777 <sqz777isme@gmail.com>
2018-07-22 13:54:40 +0800

    handle error

--- PlayGround/ClientApp/src/app/message.service.ts
+++ PlayGround/ClientApp/src/app/message.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Message } from './message';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -17,11 +17,31 @@ export class MessageService {
 
   constructor(private http: HttpClient) { }
   getMsgs(): Observable<Message[]> {
-    return this.http.get<Message[]>(this.messageUrl);
+    return this.http.get<Message[]>(this.messageUrl).pipe(
+      catchError(this.handleError<Message[]>('getMessage'))
+    );
   }
 
   addMsg(msg: Message): Observable<Message> {
-    return this.http.post<Message>(this.messageUrl, msg, httpOptions);
+    return this.http.post<Message>(this.messageUrl, msg, httpOptions).pipe(
+      catchError(this.handleError<Message>('addMsg'))
+    );
   }
 
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
