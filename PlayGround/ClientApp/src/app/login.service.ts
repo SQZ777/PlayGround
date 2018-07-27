bf19509f9dee4dd47f6dec9d90c1e0596468c523
sqz777 <sqz777isme@gmail.com>
2018-07-28 00:32:56 +0800

    not complete but can hit api!

--- PlayGround/ClientApp/src/app/login.service.ts
+++ PlayGround/ClientApp/src/app/login.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../app/objects/user';
 import { Observable, of } from '../../node_modules/rxjs';
-import { catchError } from '../../node_modules/rxjs/operators';
-import { errorHandler } from '../../node_modules/@angular/platform-browser/src/browser';
+import { BaseService } from './base.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -16,29 +15,10 @@ export class LoginService {
 
   private userUrl = '';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private baseService: BaseService) { }
 
-  postUser(user: User): Observable<User> {
-    return this.http.post<User>(this.userUrl, user, httpOptions).pipe(
-      catchError(this.handleError<User>('postUser'))
-    );
-  }
-
-
-  /**
- * Handle Http operation that failed.
- * Let the app continue.
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
+  login(user: User): Observable<User> {
+    return this.baseService.postRequest(this.userUrl, user, 'login');
   }
 }
--- PlayGround/ClientApp/src/app/login/login.component.ts
+++ PlayGround/ClientApp/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../objects/user';
-
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,11 +11,15 @@ export class LoginComponent implements OnInit {
 
   user = new User;
 
-  constructor() { }
+  constructor(private loginService: LoginService,
+    private router: Router) { }
 
   ngOnInit() {
   }
-
+  // this.router.navigateByUrl('/messageList')
   onSubmit() {
+    this.loginService.login(this.user).subscribe(() => console.log('api has hit'),
+      error => alert(error),
+      () => alert(data));
   }
 }
